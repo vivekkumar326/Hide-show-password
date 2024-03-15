5cb23d6fd88cd43dbd80594fc827ee41c3130390
vivek kumar <130165788+vivekkumar326@users.noreply.github.com>
2024-03-15 21:07:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let eyeicon=document.getElementById("eyeicon");
+// let password=document.getElementById("password");
+
+eyeicon.onclick= function()
+{
+
+    if(password.type=="password")
+    {
+        password.type="text";
+        eyeicon.src="open1.png";
+
+    }
+    else
+    {
+        password.type="password";
+        eyeicon.src="close.png";
+    }
+}
